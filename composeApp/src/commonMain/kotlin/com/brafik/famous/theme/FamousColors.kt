766d23bd4b4186e686a2f8f997068b5a721ba0f3
bunyod.rafikov <bunirafik@gmail.com>
2024-12-22 21:51:34 +0100

    Update to personal colors

---
+++ composeApp/src/commonMain/kotlin/com/brafik/famous/theme/FamousColors.kt
@@ -0,0 +1,21 @@
+package com.brafik.famous.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
+
+data class FamousColors(
+    val primaryText: Color,
+    val primaryBackground: Color,
+    val secondaryText: Color,
+    val secondaryBackground: Color,
+    val tintColor: Color
+)
+
+object FamousTheme {
+    val colors: FamousColors
+        @Composable
+        get() = LocalFamousColors.current
+}
+
+val LocalFamousColors = staticCompositionLocalOf <FamousColors> { error("No default implementation for colors") }
