2dd54be26b14cbc18774b275a438eec2dbcf3258
bunyod.rafikov <bunirafik@gmail.com>
2024-12-22 22:29:57 +0100

    Fix the theme

--- composeApp/src/commonMain/kotlin/com/brafik/famous/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/com/brafik/famous/theme/Theme.kt
@@ -1,7 +1,12 @@
 package com.brafik.famous.theme
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 
 internal val LocalThemeIsDark = compositionLocalOf { mutableStateOf(true) }
 
@@ -13,9 +18,14 @@ internal fun AppTheme(
     val isDarkState = remember(systemIsDark) { mutableStateOf(systemIsDark) }
     CompositionLocalProvider(
         LocalThemeIsDark provides isDarkState,
-        LocalFamousColors provides darkPalette,
-        content = content
-    )
+        LocalFamousColors provides darkPalette
+    ) {
+        Surface {
+            Box(Modifier.fillMaxSize().background(FamousTheme.colors.primaryBackground)) {
+                content()
+            }
+        }
+    }
 }
 
 @Composable
