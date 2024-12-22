b188aed3093e146e21494a78016cac4f614fe662
bunyod.rafikov <bunirafik@gmail.com>
2024-12-23 00:19:51 +0100

    Add initial events

--- composeApp/src/commonMain/kotlin/com/brafik/famous/features/login/models/LoginEvent.kt
+++ composeApp/src/commonMain/kotlin/com/brafik/famous/features/login/models/LoginEvent.kt
@@ -1,3 +1,6 @@
 package com.brafik.famous.features.login.models
 
-class LoginEvent
+sealed class LoginEvent {
+    class EmailChanged(val newValue: String): LoginEvent()
+    class PasswordChanged(val newValue: String): LoginEvent()
+}
